d6bda4dddc8c0c6f7131851c1bbf4146b18406c7
LeeCherry731 <sangevol0@gmail.com>
2021-01-18 20:35:22 +0700

    ...

--- javaScript Basic/ข้อที่ 2/1.js
+++ javaScript Basic/ข้อที่ 2/1.js
@@ -1,110 +1,21 @@
-let input1 = +prompt("Enter first : ");
-let input2 = +prompt("Enter second : ");
+let a = prompt("Enter A: ");
+let b = prompt("Enter B: ");
 
-
-
-const recusion = (a, b) => {
-
-    let aa = a;
-    let bb = b;
-
-    if (a === 1 || b === 1 ) {
-        return aa + "/" + bb;
-    } else {
-        if (a % 2 === 0 && b % 2 === 0) {
-            aa =  a/ 2
-            bb =  b/ 2
-            recusion(aa,bb);
-        } else if (a % 3 === 0 && b % 3 === 0) {
-            aa =  a/ 3
-            bb =  b/ 3
-            recusion(aa,bb);
-        } else if (a % 4 === 0 && b % 4 === 0) {
-            aa =  a/ 4
-            bb =  b/ 4
-            recusion(aa,bb);
-        } else if (a % 5 === 0 && b % 5 === 0) {
-            aa =  a/ 5
-            bb =  b/ 5
-            recusion(aa,bb);
-        } else if (a % 6 === 0 && b % 6 === 0) {
-            aa = a/ 6
-            bb = b/ 6
-            recusion(aa,bb);
-        } else if (a % 7 === 0 && b % 7 === 0) {
-            aa =  a/ 7
-            bb =  b/ 7
-            recusion(aa,bb);
-        } else if (a % 8 === 0 && b % 8 === 0) {
-            aa =  a/ 8
-            bb =  b/ 8
-            recusion(aa,bb);
-        } else if (a % 9 === 0 && b % 9 === 0) {
-            aa =  a/ 9
-            bb =  b/ 9
-            recusion(aa,bb);
-        } else if (a % 10 === 0 && b % 10 === 0) {
-            aa =  a/ 10
-            bb =  b/ 10
-            recusion(aa,bb);
-        } else {
-            return `${aa} / ${bb}`;
-        }
+const fn = (a, b) => { // a = 10 , b = 20
+    while (b) {
+        let x = b; // t = 20
+            b = a % b; // b = 10 % 20 = 10
+            a = x; // a = 20
     }
-    
-    
-    
-    // if (a % 2 === 0 && b % 2 === 0) {
-    //     a =  a/ 2
-    //     b =  b/ 2
-    //     recusion(a,b);
-    // } else if (a % 3 === 0 && b % 3 === 0) {
-    //     a =  a/ 3
-    //     b =  b/ 3
-    //     recusion(a,b);
-    // } else if (a % 4 === 0 && b % 4 === 0) {
-    //     a =  a/ 4
-    //     b =  b/ 4
-    //     recusion(a,b);
-    // } else if (a % 5 === 0 && b % 5 === 0) {
-    //     a =  a/ 5
-    //     b =  b/ 5
-    //     recusion(a,b);
-    // } else if (a % 6 === 0 && b % 6 === 0) {
-    //     a = a/ 6
-    //     b = b/ 6
-    //     recusion(a,b);
-    // } else if (a % 7 === 0 && b % 7 === 0) {
-    //     a =  a/ 7
-    //     b =  b/ 7
-    //     recusion(a,b);
-    // } else if (a % 8 === 0 && b % 8 === 0) {
-    //     a =  a/ 8
-    //     b =  b/ 8
-    //     recusion(a,b);
-    // } else if (a % 9 === 0 && b % 9 === 0) {
-    //     a =  a/ 9
-    //     b =  b/ 9
-    //     recusion(a,b);
-    // } else {
-    //     return `${a} / ${b}`;
-    // }
-
-    
-
-
-    // for (num in nums) {
-    //     if (num % a === 0 && num % b === 0) {
-    //         a / num;
-    //         b / num;
-    //         recusion(a, b);
-    //     } else {
-    //         return `${a} / ${b}`;
-    //     }`
-    // }
+    return a;
 }
 
-const result = recusion(input1, input2);
+let result = fn(a, b);
 
-alert(result)
-console.log(result)
+if (result === 1) {
+    alert(a + "/" + b)
+} else {
+    a = a / result;
+    b = b / result;
+    alert(a + "/" + b)
+}
