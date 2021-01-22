1d049f771a3363f70e4e3ed6a671c31300da5567
LeeCherry731 <sangevol0@gmail.com>
2021-01-22 15:48:35 +0700

    finished 7.js

--- javaScript Advanced/ข้อที่ 2/7.js
+++ javaScript Advanced/ข้อที่ 2/7.js
@@ -1,3 +1,26 @@
-const numberOfSquare = (height, width) => {
-    let test;
-}
+const gcd = (x, y) => {
+    if ((typeof x !== 'number') || (typeof y !== 'number')) 
+      return false;
+    x = Math.abs(x);
+    y = Math.abs(y);
+    while(y) {
+      var t = y;
+      y = x % y;
+      x = t;
+    }
+    return x;
+  }
+  
+const NumberOfSquares = (x, y) => { 
+
+    let s = gcd(x, y); 
+
+    let ans = (x * y) / (s * s); 
+
+    return ans; 
+} 
+
+console.log(NumberOfSquares(15, 15))
+console.log(NumberOfSquares(20, 15))
+console.log(NumberOfSquares(2, 8))
+console.log(NumberOfSquares(1, 5))
